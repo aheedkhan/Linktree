07952e7733f8bfcf6229e3deb3345c1c4f1dd63a
aheed <aheedkhan03@gmail.com>
2023-12-31 02:41:03 +0500

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import backgroundImage from "./assets/aa.jpg";
 function App() {
   return (
     <main
-      className="flex min-h-screen flex-col items-center justify-between p-34 select-none bg-cover bg-center "
+      className="flex min-h-screen flex-col items-center justify-between p-36 select-none bg-cover bg-center "
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <section>
         {" "}
-        <div className=" self-center h-96 w-96  flex flex-col  -mt-16">
+        <div className=" self-center h-96 w-96  flex flex-col  -mt-20">
           {" "}
           <div className=" self-center flex flex-col ">
             {" "}
